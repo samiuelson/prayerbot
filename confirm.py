ef07cd63fa1228be1785ce6aa927b2ccf74c4bb6
Arkadiusz Kozieł <arkadiusz.koziel@o2.pl>
2016-11-22 18:15:47 +0100

    Option to delete old prayers added

--- confirm.py
+++ confirm.py
@@ -7,9 +7,26 @@ from facebook import utils
 from translations.user import user_gettext
 from facebook.api import FacebookApi
 from events import PrayerEvent
+import time
 
 def confirm_praying_for_intention():
 
+    # for debugging
+    #print "***********************************\n";
+    #print int(time.time())-60*60*24*30
+    #print time.ctime( int(time.time()-60*60*24*30 ))
+    #print "\n";
+
+    # Delete old intentions - older than 30 days - 60 seconds * 60 minutes * 24 hours * 30 days
+    query_prayers = Intent.query.filter(Intent.ts < int(time.time())-60*60*24*30).all();
+    for prayer in query_prayers:
+        db.session.delete(prayer)
+
+    db.session.commit()
+    db.session.flush()
+
+
+    # Send reminder
     query_prayers = Intent.query.filter( Intent.commiter_id > 0, Intent.confirmed == 0 ).all();
 
     for prayer in query_prayers:
@@ -36,7 +53,7 @@ def confirm_praying_for_intention():
             api = FacebookApi();
             api.post("/me/messages", response);
 
-        #print "\n";
+
 
 if __name__ == '__main__':
     confirm_praying_for_intention()
